061864a3587cdb503c96eb5b5019ab477d271573
Krishnan Manivannan <nkhm@umich.edu>
2015-12-27 02:05:50 -0500

    Changed homepage background transparency

--- Home/Home.js
+++ Home/Home.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	$(".home_page").fadeIn(500);
-	$("#background").delay(300).fadeTo(500, 0.7);
+	$("#background").delay(300).fadeTo(500, 0.3);
 	$("#logo").delay(400).fadeIn(500);
 	$("#welcome").delay(500).fadeIn(500);
 	$(".line").delay(600).fadeIn(500);
